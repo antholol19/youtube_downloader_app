10c55cc8f55d0f5b8e1e47f2e3d7a46f90d91136
Anthony Martin <antholol19@gmail.com>
2023-03-20 10:58:32 -0400

    remove comments

--- __main__.py
+++ __main__.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 def handle_url():
     if request.method == "POST":
         link = request.form.get("url")
-        yt = downloader.Audio_downloader(link) #'https://www.youtube.com/watch?v=Iiil491G9XU' https://www.youtube.com/watch?v=-EB5NhI2RQQ
+        yt = downloader.Audio_downloader(link)
         yt.Download_Audio()
         #path = yt.title+".mp4"
         #clip = Clip(path, 40, 60)
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run() 
