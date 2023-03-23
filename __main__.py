d135fb91f46f993a6b7c10c9fb29d4f31851b110
Anthony Martin <antholol19@gmail.com>
2023-03-23 13:30:56 -0400

    add action buttons to handle for Flask

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, redirect, url_for, render_template, request
 import downloader
 
@@ -5,13 +6,17 @@ import downloader
 app = Flask(__name__)
 
 @app.route('/', methods =["GET", "POST"])
-def handle_url():
+def handle_download():
     if request.method == "POST":
-        link = request.form.get("url")
-        yt = downloader.Audio_downloader(link)
-        yt.Download_Audio()
-        #path = yt.title+".mp4"
-        #clip = Clip(path, 40, 60)
+        link = request.form.get('url')
+        yt = downloader.Downloader(link)
+        yt.output_path()
+        if request.form['audio'] == "yes":
+            yt.Download_Audio_only()
+        if request.form['video'] == "yes":
+            yt.Download_Video_only()
+        if request.form['audio'] == "no" and request.form['video'] == 'no':
+            yt.Download_Video()
     return render_template('home.html')
 
 if __name__ == "__main__":
